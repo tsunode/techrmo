c178174fd58886255ba10456b1a8d12b772eae9a
alvseven <alv7contato@outlook.com>
2023-04-26 01:50:04 -0300

    testing key component

--- src/modules/game/components/Keyboard/Key/index.tsx
+++ src/modules/game/components/Keyboard/Key/index.tsx
@@ -1,7 +1,38 @@
-import styles from './styles.module.scss';
+import { ChangeEvent,KeyboardEvent, useRef } from 'react';
 
-export const Key = () => (
-  <div className={styles.container}>
-    teste
-  </div>
-);
+import { container } from './styles.module.scss';
+
+export const Key = () => {
+
+  const ref = useRef<HTMLInputElement | null>(null)
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    
+    const nextInput = event.target.nextElementSibling as HTMLInputElement // unfortunately we need a type assertion here, cause even if we check that nextElementSibling is not null and is a input, it still will be inferred as Element instead of HTMLInputElement, so with type assertion we can use .focus()
+    
+    if(event.target.value && nextInput && nextInput.tagName === 'INPUT'){
+      nextInput.focus()
+    }
+  }
+
+  const handleKeyUp = (event:KeyboardEvent<HTMLInputElement>) => {
+    
+    const previousInput =  event.currentTarget?.previousElementSibling as HTMLInputElement 
+    const lastInput = event.currentTarget.parentNode?.lastElementChild as HTMLInputElement
+    const elementToFocus = previousInput?.tagName === "INPUT" ? previousInput : lastInput?.tagName === "INPUT" ? lastInput : null
+
+    if(event.key === "Backspace" && elementToFocus && event.currentTarget.value === "") {
+        // event.currentTarget.value === "" will not be needed when we ensure that the maximum length of the input value is 1
+        elementToFocus.focus();
+    } 
+  }
+
+  return <input 
+  className={container} 
+  type="text" 
+  ref={ref} 
+  onChange={(event) => {handleChange(event)}} 
+  onKeyUp={(event) => handleKeyUp(event)}/>
+  
+}
+ 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,8 +10,14 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <h1>teste</h1>
-      <Key />
+      <h1>Testing a basic functionality :D</h1>
+      <div style={{display: 'flex', margin: '20px', gap: "10px"}}> 
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      </div>
     </>
   );
 }
